e3921ffb960b4a824edd67daf9fc40632eea2077
mghiyats <163238879+mghiyats@users.noreply.github.com>
2024-03-18 02:19:18 +0700

    Program.cs

--- tpmodul5_1302220109/Program.cs
+++ tpmodul5_1302220109/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class HaloGeneric
+{
+    public static void SapaUser(string nama)
+    {
+        Console.WriteLine("Halo user {0}", nama);
+    }
+}
+
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+
+        HaloGeneric.SapaUser("Fatih");
+
+
+        DataGeneric<int> dataNIM = new DataGeneric<int>(1302220109);
+        dataNIM.PrintData();
+    }
+}
